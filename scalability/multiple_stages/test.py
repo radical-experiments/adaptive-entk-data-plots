4ffd8b63e0c74e663bf5228d26ec8b08cc51df13
vivek-bala <vivek.balasubramanian@rutgers.edu>
2017-01-20 13:47:55 -0500

    pushing plot

--- scalability/multiple_stages/test.py
+++ scalability/multiple_stages/test.py
@@ -11,19 +11,47 @@ if __name__ == '__main__':
     cores = 256
     stages = [2,3,4,5]
 
-    f = open('multi-stage-profiles.csv','w')
-    f.write('tasks, cores, execution time\n')
 
     for cnt in range(len(stages)):
         src = 'stages_%s'%(stages[cnt])
-        sid=glob.glob('./%s/*.json'%(src))[0].split('/')[2][:-5]
+
+        sid = glob.glob('./%s/*.json'%(src))[0].split('/')[2][:-5]
         session = ra.Session(sid, 'radical.pilot', src=src)
 
         units = session.get(etype='unit')
 
-        units = session.filter(etype='unit', inplace=False)
-        exec_time = units.duration(state=[rp.AGENT_EXECUTING, rp.AGENT_STAGING_OUTPUT_PENDING])
-        
-        f.write('%s, %s, %s\n'%(tasks, cores, exec_time))
+        json_file = './%s/'%src + sid + '.json'
+        json = ru.read_json(json_file)
 
-    f.close()
+        states = units[0].states.keys()
+
+        f = open('./%s/prof-%s.csv'%(src,sid),'w')
+
+        header = 'uid, cores, executable,'
+        for state in states:
+            header += ' ' + str(state) + ','
+        f.write(header +'\n')
+
+        print states
+
+        print '-----------------------------------------------------------'
+
+        for u1 in units:
+            uid = u1.uid
+
+            for u2 in json['unit']:
+                if uid == u2['_id']:            
+                    cores = u2['description']['cores']
+                    executable = u2['description']['executable']
+                    break
+
+            line = str(uid) + ', ' + str(cores) + ', ' + executable
+
+            for state in states:
+                line += ', ' + str(u1.states[state]['time']) 
+
+            line = line + '\n'
+
+            f.write(line)
+
+        f.close()
